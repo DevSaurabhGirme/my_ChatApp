6635b7c8d7098d44307d4747377c1cb04e53fef5
Saurabh <sygirme13@gmail.com>
2024-05-16 22:10:22 +0530

    chatbox container layout created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+const style = {
+  appContainer : `max-w-[728px] mx-auto text-center`,
+  sectionContainer : `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`
+}
+
 function App() {
   return (
-    <div>
-      
+    <div classsName={style.appContainer}>
+      <section className={style.sectionContainer}>
+        
+      </section>
     </div>
   );
 }
