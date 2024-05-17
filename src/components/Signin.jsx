19e7cc9df077ddc528a0f2a5d39362fe01791ce1
Saurabh <sygirme13@gmail.com>
2024-05-17 22:09:21 +0530

    GoogleSignIn function defined and Google button action initiated

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,16 +1,22 @@
+import { signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
 import React from "react";
+import {auth} from '../firebase'
 import GoogleButton from "react-google-button";
 
 const style = {
     wrapper: `flex justify-center`
 }
 
-
+const googleSignIn = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithRedirect(auth, provider)
+}
 
 const SignIn = () => {
     return (
         <div className={style.wrapper}>
-            <GoogleButton />
+            <GoogleButton onClick={googleSignIn}/>
         </div>
     )
 }
