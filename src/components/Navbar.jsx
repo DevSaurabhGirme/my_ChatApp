371337370c920fe7d9ea6955b1ed3564a9ba129c
Saurabh <sygirme13@gmail.com>
2024-05-17 21:53:11 +0530

    firebase imports added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import {auth} from '../firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
+
 
 const style = {
     nav : `bg-gray-600 h-20 flex justify-between items-center p-4`,
@@ -6,6 +9,7 @@ const style = {
 }
 
 const Navbar = () => {
+    const [user] = useAuthState(auth)
     return (
         <div className={style.nav}>
             <h1 className={style.heading}>Chat App</h1>
