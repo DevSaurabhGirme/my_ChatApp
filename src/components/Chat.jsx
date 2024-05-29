e096fc41af135c1d2f2da24feb5175552ddff0b8
Saurabh <sygirme13@gmail.com>
2024-05-29 22:06:04 +0530

    chat component added

---
+++ src/components/Chat.jsx
@@ -0,0 +1,25 @@
+import React, {useState, useEffect, useRef} from "react";
+import Message from "./Message";
+
+const style = {
+    main : `flex flex-col p-[10px] relative`
+};
+
+const Chat = () => {
+const scroll = useRef()
+
+    return (
+        <>
+            <main className={StylePropertyMap.main}> 
+                <Message />
+            </main>
+            <span ref ={scroll}></span>
+            
+        </>
+        
+            
+        
+    )
+}
+
+export default Chat;
