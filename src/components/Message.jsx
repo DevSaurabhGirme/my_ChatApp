656ba93b435ccfc164731ff7ce55329e0c250127
Saurabh <sygirme13@gmail.com>
2024-05-30 22:58:42 +0530

    hard coded conversation message tested

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 const style = {
     Message : `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+    name: `fixed mt-[-4rem]text-gray-600 text-xs`,
+    sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+    received: `bg-[#e5e5a] text-black float-left rounded-br-full`
 }
 
 const Message = () => {
