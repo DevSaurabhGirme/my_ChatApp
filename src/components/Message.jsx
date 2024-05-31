422f291b2cbfd01ad250b504cfbc0b147f1d837d
Saurabh <sygirme13@gmail.com>
2024-05-31 22:30:06 +0530

    username and message overlapping issue solved

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 const style = {
-    Message : `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-    name: `fixed mt-[-4rem]text-gray-600 text-xs`,
+    message : `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+    name: `fixed mt-[-4rem] text-gray-600 text-xs`,
     sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
-    received: `bg-[#e5e5a] text-black float-left rounded-br-full`
+    received: `bg-[#e5e5ea] text-black float-left rounded-br-full`
 }
 
 const Message = ({message}) => {
     return (
         <div>
-            <div className ={style.Message}>
+            <div className ={style.message}>
                 <p className={style.name}>Dave</p>
                 <p>{message.text}</p>
             </div>
