916dbea4ee991cf0bc66c3cb2f72baef7d9bd6af
Saurabh <sygirme13@gmail.com>
2024-05-31 15:30:01 +0530

    real-time listener added to Firestore db collection

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,27 @@
 import React, {useState, useEffect, useRef} from "react";
 import Message from "./Message";
+import {query, collection, onSnapshot, orderBy } from "firebase/firestore";
+import {db} from "../firebase"
 
 const style = {
     main : `flex flex-col p-[10px] relative`
 };
 
 const Chat = () => {
-const scroll = useRef()
+    const [messages, setMessages] = useState([]);
+    const scroll = useRef();
+
+    useEffect(() => {
+        const q = query(collection(db,'messages'), orderBy('timestamp'))
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            let messages = [];
+            querySnapshot.forEach((doc) => {
+                messages.push({...doc.data(), id : doc.id})
+            });
+            setMessages(messages);
+        });
+        return () => unsubscribe();
+    },[]);
 
     return (
         <>
