abc7ac594e6c570ff3908ea4905259677038e698
Saurabh <sygirme13@gmail.com>
2024-06-03 23:53:26 +0530

    button style update

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import {auth} from '../firebase'
+import { auth } from '../firebase';
 
 const style = {
-    button : `bg-gray-200 px-4 py-2 hover:bg-gray=100`
+    button: `bg-gray-200 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 transition duration-300 ease-in-out`,
 }
 
 const Logout = () => {
-
     const signOut = () => {
-        signOut(auth)
+        signOut(auth);
     }
 
     return (
-        <button onClick = {() => auth.signOut()}className={style.button}>
+        <button onClick={signOut} className={style.button}>
             Logout
         </button>
-    )
+    );
 }
 
-export default Logout
+export default Logout;
